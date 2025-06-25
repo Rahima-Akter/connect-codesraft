0b2baffb2361be86eba2c8ae348964b56e4a2703
Rahima-Akter <rahimaakter1527@gmail.com>
2025-06-25 12:48:22 +0600

    services card section done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,20 +1,11 @@
-import bg from "../assets/banner.jpg";
+import Banner from "./Banner";
+import OurServices from "./OurServices";
 
 const Home = () => {
-  return (
-    <div className="w-full overflow-hidden relative">
-      <img src={bg} alt="banner" className="block w-[100vw] lg:h-[100vh] md:h-[45vh] h-[90vh]" />
-
-      {/* banner contents */}
-      <div className="absolute top-32 md:left-5 text-black">
-        <h1 className="uppercase font-extrabold text-center md:text-6xl text-4xl text-[#263524] lg:px-[350px] mx-auto md:px-0 px-5">we connect your business</h1>
-
-        <p className="capitalize text-center lg:px-96 md:px-40 px-16 font-semibold mt-5 text-gray-700">welcome to our telecom website, where we provide <br/> hight-quality services to meet your communication needs. our company is comitted to offering the latest technology and the best prices in the market</p>
-
-        <button className="capitalize font-bold rounded-full bg-[#1f352d] hover:bg-[#2b463c] hover:cursor-pointer px-6 py-3 text-white flex justify-center mx-auto mt-8">get started</button>
-      </div>
-    </div>
-  );
+  return <div className="bg-[#ecf5e9]">
+    <Banner/>
+    <OurServices/>
+  </div>;
 };
 
 export default Home;
---
+++ src/components/OurServices.jsx
@@ -0,0 +1,77 @@
+import speed_icon from "../assets/speed-radar.png";
+import antenna from "../assets/antenna.png";
+import cloude from "../assets/cloude.png";
+import low_cost from "../assets/low-cost.png";
+import solution from "../assets/solution.png";
+import video_conf from "../assets/video-conf.png";
+import { data } from "react-router-dom";
+
+const OurServices = () => {
+  const card_info = [
+    {
+      icon: speed_icon,
+      heading: "we provide highest internet speed",
+      description:
+        "With our fiber optice network we provide the fastest download and upload speed. So you can get your work done quickly and efficiently",
+    },
+    {
+      icon: low_cost,
+      heading: "we give services at very low cost",
+      description:
+        "Our prices are competitive, and we also offer promotions and discounts to ensure that you get the best value for your money",
+    },
+    {
+      icon: antenna,
+      heading: "highest intenet coverage ever",
+      description:
+        "with our fiber optic network, we provide the fastest download and upload speeds, so you can get your work done quickly and efficiently.",
+    },
+    {
+      icon: cloude,
+      heading: "unlimited cloude- based solutions",
+      description:
+        "We offer cloude-based storage and backup solutions for customers to store important documents files",
+    },
+    {
+      icon: video_conf,
+      heading: "video conferencing solutions at ease",
+      description:
+        "Our prices are competitive, and we also offer promotions and discounts to ensure that you get the best value for your money",
+    },
+    {
+      icon: solution,
+      heading: "faster solutions to your problems",
+      description:
+        "with our fiber optic network, we provide the fastest download and upload speeds, so you can get your work done quickly and efficiently.",
+    },
+  ];
+  return (
+    <div className="lg:py-20 py-12 w-11/12 mx-auto">
+      <h1 className="uppercase font-extrabold text-center md:text-5xl text-4xl text-[#263524] lg:px-[350px] mx-auto md:px-32 px-5">
+        why should chooose us
+      </h1>
+
+      <p className="text-center lg:px-80 md:px-52 px-10 font-medium text-sm mt-2 text-gray-700">
+        We offer a variety of Services to our customers, including phone plans,
+        internet packages. and TV bundles. Our phone plans offer unlimited talk
+        and text, as well as varying amounts of data depending on your needs.
+      </p>
+      {/* cards */}
+      <div className="grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 gap-5 mt-12">
+        {card_info.map((card) => (
+          <div key={card.icon} className="border-2 border-[#263524] py-6 px-5">
+            <img
+              src={card.icon}
+              className="w-20 -mt-1"
+              alt={data.icon}
+            />
+            <h2 className="font-bold uppercase text-3xl pb-3 pt-8">{card.heading}</h2>
+            <p className="font-medium">{card.description}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default OurServices;
